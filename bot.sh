c5bf3dc145c97e63eb42906fabc1574940b5ef06
Lauri <lauri.ajanki@kolumbus.fi>
2015-09-05 01:28:29 +0300

    Non-recursive launcher

---
+++ bot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A launcher script to run quote.py. Attempts to run it again if the
+# first try is unsuccessful.
+
+python ./youtube_search.py --bot
+
+ret=$?
+if [ $ret -ne 0 ]; then
+  echo "Not a valid quote, trying again..."
+  python ./youtube_search.py --bot
+fi
+echo $(date +"%d.%m.%Y-%H:%M")
