2e7ffd495ea10d8707984ccbe1be545efd5ec1bf
Lauri Ajanki <laareni@yahoo.co.uk>
2017-11-03 21:36:56 +0200

    Refactored into a class

--- tests/test_twitterbot.py
+++ tests/test_twitterbot.py
@@ -20,18 +20,11 @@ class SearchTermIndexTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         """Set up a test database with dummy data."""
-        self.con = sqlite3.connect("./test.db")
-        self.cur = self.con.cursor()
+        self.app = twitterbot.Bot("./test.db")
 
-        with self.con:
-            self.cur.execute("CREATE TABLE search_terms(search_term TEXT)")
-            self.cur.execute("CREATE TABLE links(url TEXT, date TEXT, title TEXT, views INTEGER)")
-
-            #dummy_search_terms = [("agamemnon",), ("molenCia",), ("hardly different",)]
-            #self.cur.executemany("INSERT INTO search_terms VALUES (?)", dummy_search_terms)
-
-            dummy_links = [("url1", "date1", "title1", "views1"), ("url2", "date2", "title2", "views2")]
-            self.cur.executemany("INSERT INTO links VALUES (?, ?, ?, ?)", dummy_links)
+        with self.app.con:
+            self.app.cur.execute("CREATE TABLE search_terms(search_term TEXT)")
+            self.app.cur.execute("CREATE TABLE links(url TEXT, date TEXT, title TEXT, views INTEGER)")
 
     @classmethod
     def tearDownClass(self):
@@ -39,10 +32,10 @@ class SearchTermIndexTestCase(unittest.TestCase):
 
     def testFilledAfterRefresh(self):
         """Is the search term index non empty after a refresh?"""
-        twitterbot.refresh_index()
-        with self.con:
-            self.cur.execute("SELECT COUNT(search_term) FROM search_terms")
-            response = self.cur.fetchone()
+        self.app.refresh_index()
+        with self.app.con:
+            self.app.cur.execute("SELECT COUNT(search_term) FROM search_terms")
+            response = self.app.cur.fetchone()
 
         self.assertNotEqual(response, [0], "index is empty")
 
@@ -52,34 +45,34 @@ class SearchTermIndexTestCase(unittest.TestCase):
         2 further attempt raises an error
         """
         # initialize the index with dummy data
-        with self.con:
-            self.cur.execute("DELETE FROM search_terms")
+        with self.app.con:
+            self.app.cur.execute("DELETE FROM search_terms")
             dummy_search_terms = [("agamemnon",), ("molenCia",), ("hardly different",)]
-            self.cur.executemany("INSERT INTO search_terms VALUES (?)", dummy_search_terms)
+            self.app.cur.executemany("INSERT INTO search_terms VALUES (?)", dummy_search_terms)
 
-        search_terms = twitterbot.get_search_term_slice(3)
+        search_terms = self.app.get_search_term_slice(3)
 
         # get the size of the index
-        with self.con:
-            self.cur.execute("SELECT COUNT(search_term) FROM search_terms")
-            size = self.cur.fetchone()[0]
+        with self.app.con:
+            self.app.cur.execute("SELECT COUNT(search_term) FROM search_terms")
+            size = self.app.cur.fetchone()[0]
         self.assertEqual(size, 0, "Items were not removed from the search term index")
 
         # next call to get_search_term_slice should raise an error
-        self.assertRaises(twitterbot.IndexEmptyException, twitterbot.get_search_term_slice, 1)
+        self.assertRaises(twitterbot.IndexEmptyException, self.app.get_search_term_slice, 1)
 
     def testRefreshedAfterSliceOnEmpty(self):
-        """Is the search term index automatically filled if trying to pop on empty table?"""
+        """Is the search term index automatically filled if trying to pop from empty table?"""
         # empty the index
-        with self.con:
-            self.cur.execute("DELETE FROM search_terms")
+        with self.app.con:
+            self.app.cur.execute("DELETE FROM search_terms")
 
-        # attempt to read from the index
-        twitterbot.parse_new_links(5)
+        # attempt to read from the index, should result in a refresh
+        self.app.parse_new_links(5)
 
         # get the size of the index
-        with self.con:
-            self.cur.execute("SELECT COUNT(search_term) FROM search_terms")
-            response = self.cur.fetchone()
+        with self.app.con:
+            self.app.cur.execute("SELECT COUNT(search_term) FROM search_terms")
+            response = self.app.cur.fetchone()
 
         self.assertNotEqual(response, [0], "Index did not get refreshed on empty")
--- twitterbot.py
+++ twitterbot.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Uses youtube_search.py to parse for links to zer oview items and tweets them one at a time.
+Uses youtube_search.py to parse for links to zero view items and tweets them one at a time.
 """
 
 import os
@@ -15,21 +15,16 @@ import codecs
 import youtube_search
 
 
-# setup a folder for bot datafiles
-base = "bot-data/"
-links_storage = base + "links.db"
-if not os.path.isdir(base):
-  os.mkdir(base)
 
-con = sqlite3.connect(links_storage)
-cur = con.cursor()
 
 # silence verbose http logging done by requests via urllib3
 requests_log = logging.getLogger("requests")
 requests_log.addHandler(logging.NullHandler())
 requests_log.propagate = False
 
-#logging.getLogger("urllib3").setLevel(logging.WARNING)
+urlib_log = logging.getLogger("urllib3")
+urlib_log.addHandler(logging.NullHandler())
+urlib_log.propagate = False
 
 # setup actual loggig
 logging.basicConfig(
@@ -40,169 +35,177 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def init_bot():
-  """Initialize the bot by creating a link storage database with a search term index table."""
-  with con:
-    cur.execute("CREATE TABLE IF NOT EXISTS search_terms(search_term TEXT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS links(url TEXT, date TEXT, title TEXT, views INTEGER)")
+class Bot:
+    def __init__(self, path):
+        self.con = sqlite3.connect(path)
+        self.cur = self.con.cursor()
 
-  refresh_index()
+    def create_tables(self):
+        """Initialize the bot by creating a link storage database with a search term index table."""
+        with self.con:
+            self.cur.execute("CREATE TABLE IF NOT EXISTS search_terms(search_term TEXT)")
+            self.cur.execute("CREATE TABLE IF NOT EXISTS links(url TEXT, date TEXT, title TEXT, views INTEGER)")
 
-def refresh_index():
-  """Refill the index table from dict.txt. Drops previous data."""
-  with codecs.open("./dict.txt", encoding="utf-8") as f:
-    search_terms = f.read().splitlines()
-    search_terms = [(item,) for item in search_terms]  # format as list of tuples to be able to pass to executemany
+        self.refresh_index()
 
-  with con:
-    cur.execute("DELETE FROM search_terms")
-    cur.executemany("INSERT INTO search_terms VALUES (?)", search_terms)
+    def refresh_index(self):
+        """Refill the index table from dict.txt. Drops previous data."""
+        with codecs.open("./dict.txt", encoding="utf-8") as f:
+            search_terms = f.read().splitlines()
+            search_terms = [(item,) for item in search_terms]  # format as list of tuples to be able to pass to executemany
 
-def parse_new_links(n):
-  """Use the crawler to search for zero view videos and store them in the database.
-  Args:
-    n (int): number of items to parse: n/2 search terms are picked from the index and n/2 are randomly generated
-  """
-  app = youtube_search.VideoCrawler()
-  try:
-    search_term_slice = get_search_term_slice(n/2)
-    randomized = app.generate_random_search_terms(n/2, 2)
-    search_terms = search_term_slice + randomized
-    zeros = app.zero_search(search_terms)
+        with self.con:
+            self.cur.execute("DELETE FROM search_terms")
+            self.cur.executemany("INSERT INTO search_terms VALUES (?)", search_terms)
 
-    # Add new links to the database
-    if zeros:
-      links = [ (item["url"], item["date"], item["title"], item["views"]) for item in zeros ]
-      with con:
-        cur.executemany("INSERT INTO links VALUES (?, ?, ?, ?)", links)
-        logger.info("Added {} new links to the database".format(len(zeros)))
+    def parse_new_links(self, n):
+        """Use the crawler to search for zero view videos and store them in the database.
+        Args:
+            n (int): number of items to parse: n/2 search terms are picked from the index and n/2 are randomly generated
+        """
+        crawler = youtube_search.VideoCrawler()
+        try:
+            search_term_slice = self.get_search_term_slice(n/2)
+            randomized = crawler.generate_random_search_terms(n/2, 2)
+            search_terms = search_term_slice + randomized
+            zeros = crawler.zero_search(search_terms)
 
-    else:
-      logger.info("No new links detected")
+            # Add new links to the database
+            if zeros:
+                links = [ (item["url"], item["date"], item["title"], item["views"]) for item in zeros ]
+                with self.con:
+                    self.cur.executemany("INSERT INTO links VALUES (?, ?, ?, ?)", links)
+                    logger.info("Added {} new links to the database".format(len(zeros)))
 
-  # refresh (but don't do any parsing), on empty index
-  except IndexEmptyException as err:
-    logger.info("search term index is empty, refreshing")
-    refresh_index()
+            else:
+              logger.info("No new links detected")
 
-def get_search_term_slice(n):
-  """Return a random sample of n search terms from the database."""
-  with con:
-    cur.execute("SELECT search_term FROM search_terms ORDER BY RANDOM() LIMIT {}".format(n))
-    search_terms = cur.fetchall()
+        # refresh (but don't do any parsing), on empty index
+        except IndexEmptyException as err:
+            logger.info("search term index is empty, refreshing")
+            self.refresh_index()
 
-    # raise an error if there are no search terms left in the index
-    if not search_terms:
-      raise IndexEmptyException()
+    def get_search_term_slice(self, n):
+        """Return a random sample of n search terms from the database."""
+        with self.con:
+            self.cur.execute("SELECT search_term FROM search_terms ORDER BY RANDOM() LIMIT {}".format(n))
+            search_terms = self.cur.fetchall()
 
-    # delete the items from the database
-    cur.executemany("DELETE FROM search_terms WHERE search_term = ?", search_terms)
+            # raise an error if there are no search terms left in the index
+            if not search_terms:
+                raise IndexEmptyException()
 
-  return [item[0] for item in search_terms]  # return a flattened list of the search terms
+            # delete the items from the database
+            self.cur.executemany("DELETE FROM search_terms WHERE search_term = ?", search_terms)
 
-def get_link():
-  """Choose a random link form the database."""
-  browser = youtube_search.VideoBrowser()
-  with con:
-    # keep popping items from the database until we find one which still has no views or
-    # until the table is empty
-    views = 1
-    while views:
-      cur.execute("SELECT rowid, * FROM links ORDER BY RANDOM() LIMIT 1")
-      row = cur.fetchone()
+        return [item[0] for item in search_terms]  # return a flattened list of the search terms
 
-      if not row:
-        raise IndexError("No links in the database")
+    def get_link(self):
+        """Choose a random link form the database."""
+        browser = youtube_search.VideoBrowser()
+        with self.con:
+            # keep popping items from the database until we find one which still has no views or
+            # until the table is empty
+            views = 1
+            while views:
+                self.cur.execute("SELECT rowid, * FROM links ORDER BY RANDOM() LIMIT 1")
+                row = self.cur.fetchone()
 
-      rowid = row[0]
-      url = row[1]
+                if not row:
+                    raise IndexError("No links in the database")
 
-      # requery the view count from YouTube
-      vid_id = url.split("?v=")[1]  # get the id from the url
-      stats = browser.get_stats(vid_id)
-      views = stats["views"]
+                rowid = row[0]
+                url = row[1]
 
-      # delete the item from the table regardless of the current view count
-      cur.execute("DELETE FROM links WHERE rowid = ?", (rowid,))
+                # requery the view count from YouTube
+                vid_id = url.split("?v=")[1]  # get the id from the url
+                stats = browser.get_stats(vid_id)
+                views = stats["views"]
 
-    return (row[1], row[2], row[3], row[4])
+                # delete the item from the table regardless of the current view count
+                self.cur.execute("DELETE FROM links WHERE rowid = ?", (rowid,))
 
-def tweet():
-  """Attempts to tweet the topmost item from the links database. Prints an error message if
-  there is nothing to tweet.
-  """
-  with open("./keys.json") as f:
-    keys = json.load(f)
+            return (row[1], row[2], row[3], row[4])
 
-  API_KEY = keys["TWITTER_API_KEY"]
-  API_SECRET = keys["TWITTER_API_SECRET"]
-  OAUTH_TOKEN = keys["TWITTER_OAUTH_TOKEN"]
-  OAUTH_SECRET = keys["TWITTER_OAUTH_SECRET"]
-  twitter = twython.Twython(API_KEY, API_SECRET, OAUTH_TOKEN, OAUTH_SECRET)
+    def tweet(self):
+        """Attempts to tweet the topmost item from the links database. Prints an error message if
+        there is nothing to tweet.
+        """
+        with open("./keys.json") as f:
+          keys = json.load(f)
 
-  try:
-    link = get_link()
-    url = link[0]
-    date = link[1]
-    title = link[2]
-    views = link[3]
+        API_KEY = keys["TWITTER_API_KEY"]
+        API_SECRET = keys["TWITTER_API_SECRET"]
+        OAUTH_TOKEN = keys["TWITTER_OAUTH_TOKEN"]
+        OAUTH_SECRET = keys["TWITTER_OAUTH_SECRET"]
+        twitter = twython.Twython(API_KEY, API_SECRET, OAUTH_TOKEN, OAUTH_SECRET)
 
-    """
-    Format a message to tweet:
-    for long tweets, cut the title to 75 characters
-    url = 23 characters (after Twitter's own link shortening, see https://support.twitter.com/articles/78124)
-    date = 20
-    views ~ 8
-    linebreaks = 3
-    => title: first 70 characters + "..."
-    """
-    if len(title) > 75:
-      title = title[:72] + "..."
+        try:
+            link = self.get_link()
+            url = link[0]
+            date = link[1]
+            title = link[2]
+            views = link[3]
 
-    msg = u"{}\n{}\nuploaded: {}\nviews: {}".format(title, url, date, views)
+            """
+            Format a message to tweet:
+            for long tweets, cut the title to 75 characters
+            url = 23 characters (after Twitter's own link shortening, see https://support.twitter.com/articles/78124)
+            date = 20
+            views ~ 8
+            linebreaks = 3
+            => title: first 70 characters + "..."
+            """
+            if len(title) > 75:
+                title = title[:72] + "..."
 
-    # Encode the message for network I/O
-    msg = msg.encode("utf8")
-    twitter.update_status(status = msg)
-  except IndexError as err:
-    logger.error("links table is empty, nothing to tweet")
-  except twython.exceptions.TwythonError as err:
-    logger.error(err)
-    print err
+            msg = u"{}\n{}\nuploaded: {}\nviews: {}".format(title, url, date, views)
 
-def get_bot_status():
-    """Get the number of links and search terms in the database"""
-    with con:
-      cur.execute("SELECT COUNT(*) FROM links")
-      nlinks = cur.fetchone()[0]
+            # Encode the message for network I/O
+            msg = msg.encode("utf8")
+            twitter.update_status(status = msg)
 
-      cur.execute("SELECT COUNT(*) FROM search_terms")
-      nindex = cur.fetchone()[0]
+        except IndexError as err:
+            logger.error("links table is empty, nothing to tweet")
+        except twython.exceptions.TwythonError as err:
+            logger.error(err)
+            print err
 
-    return {"links": nlinks, "index": nindex}
+    def get_status(self):
+        """Get the number of links and search terms in the database"""
+        with self.con:
+            self.cur.execute("SELECT COUNT(*) FROM links")
+            nlinks = self.cur.fetchone()[0]
+
+            self.cur.execute("SELECT COUNT(*) FROM search_terms")
+            nindex = self.cur.fetchone()[0]
+
+        return {"links": nlinks, "index": nindex}
 
 
 class IndexEmptyException:
-  pass
+    pass
+
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description = "Search for and tweet Youtube videos with no or little views.")
-  parser.add_argument("--tweet", help = "Tweet the next result stored in links.json.", action = "store_true")
-  parser.add_argument("--parse", help = "Parse n next search terms from search_terms.json for zero view items and store them to links.json.", metavar = "n", type = int)
-  parser.add_argument("--stats", help = "Prints number of items left in links.json and index.json", action = "store_true")
-  parser.add_argument("--init", help = "Initialize the bot by creating a file structure in bot-data/", action = "store_true")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description = "Search for and tweet Youtube videos with no or little views.")
+    parser.add_argument("--tweet", help = "Tweet the next result stored in links.json.", action = "store_true")
+    parser.add_argument("--parse", help = "Parse n next search terms from search_terms.json for zero view items and store them to links.json.", metavar = "n", type = int)
+    parser.add_argument("--stats", help = "Prints number of items left in links.json and index.json", action = "store_true")
+    parser.add_argument("--init", help = "Initialize the bot by creating a file structure in bot-data/", action = "store_true")
+    args = parser.parse_args()
+    bot = Bot("bot-data/links.db")
 
-  if args.init:
-	init_bot()
+    if args.init:
+        bot.create_tables()
 
-  elif args.tweet:
-    tweet()
+    elif args.tweet:
+        bot.tweet()
 
-  elif args.parse:
-    logging.info("Parsing new links")
-    parse_new_links(args.parse)
+    elif args.parse:
+        logging.info("Parsing new links")
+        bot.parse_new_links(args.parse)
 
-  elif args.stats:
-    stats = get_bot_status()
-    print "{} links in links.json and {} search terms in index.json".format(stats["links"], stats["index"])
+    elif args.stats:
+        stats = bot.get_status()
+        print "{} links in links.json and {} search terms in index.json".format(stats["links"], stats["index"])
